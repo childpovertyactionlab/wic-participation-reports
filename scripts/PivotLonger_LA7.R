3cea5553d5b4dfe031f2c4b84e0d0804e6d29198
Michael Lopez <michael@childpovertyactionlab.org>
2022-08-02 13:01:05 -0500

    june update

--- scripts/PivotLonger_LA7.R
+++ scripts/PivotLonger_LA7.R
@@ -4,7 +4,7 @@ library(rio)
 #### Changing to Tall Format ####
 wic_localagency <- import("data/LA7 Monthly Clinic Participation Report Wide.csv") %>%
   janitor::clean_names(.) %>%
-  pivot_longer(!(site:site_name), names_to = "date", values_to = "participation") %>%
+  pivot_longer(!(site:site_name), names_to = "date", values_to = "participation", values_ptypes = list(participation = integer())) %>%
   mutate(date = str_remove(date, "x"),
          date = as.Date(paste0(date, "_01"), format = "%y_%b_%d"))
 
